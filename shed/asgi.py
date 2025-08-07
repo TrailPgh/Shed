6fc12699202ffb42c4e5fcedbfa06737dd450105
Gregory V Tomei <gvt@fastmail.com>
2025-08-06 20:51:16 -0400

    rename Shed to lowercase shed. add root path handler.

--- shed/asgi.py
+++ shed/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Shed project.
+ASGI config for shed project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Shed.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shed.settings')
 
 application = get_asgi_application()
--- shed/wsgi.py
+++ shed/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Shed project.
+WSGI config for shed project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Shed.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shed.settings')
 
 application = get_wsgi_application()
