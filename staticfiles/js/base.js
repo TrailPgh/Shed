69bd9059d176454575e9d9d3afb5cda729eafa24
Gregory V Tomei <gvt@fastmail.com>
2025-08-19 11:33:46 -0400

    add static file to git to simplify deployment for now. ran: ./manage.py collectstatic

---
+++ staticfiles/js/base.js
@@ -0,0 +1,10 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    // this is just a test and evidences that JS is working. remove later.
+    var didJsCompleteMsg = document.createElement("p");
+    didJsCompleteMsg.innerHTML = "JS executed.";
+    didJsCompleteMsg.style.color = "green";
+    didJsCompleteMsg.style.fontSize = "0.8em";
+    document.body.appendChild(didJsCompleteMsg);
+    // add any other JS here.
+});
